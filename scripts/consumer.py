95fb1d03fca323e70c7985d8d3bed48f2fa55d10
eykop <e.kopty@yahoo.com>
2020-05-09 15:11:51 +0200

    initial consumer/producer entry point scripts

--- scripts/consumer.py
+++ scripts/consumer.py
@@ -1,11 +1,73 @@
-from pprint import pprint
-from kafka_helper.consumer import Consumer
+import json
+import logging
+import argparse
+import os
+import sys
+from json import JSONDecodeError
+
+from kafka_postgres.kafka_helper.consumer import Consumer
+from kafka_postgres.postgresql_helper.postgresql_helper import PostgreSqlClient
+from kafka_postgres.settings import settings
+from kafka_postgres.settings.exceptions import ConfigurationFileNotFoundException
+
+log = logging.getLogger("kafka_postgresql_demo")
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
+
+
+def start_kafka_consumer(kafka_config: dict, postgresql_config: dict):
+    try:
+        try:
+            consumer = Consumer(**kafka_config)
+            client = PostgreSqlClient()
+            client.connect(**postgresql_config)
+        except KeyError as error:
+            consumer.close()
+            client.close()
+            log.error("Configuration value missing from config file:  %s", error)
+
+        def insert_to_tbl(data: str):
+            log.debug("message: %s", data)
+            try:
+                json_data = json.loads(data)
+                client.insert_web_monitoring_result("yahoo", json_data)
+            except JSONDecodeError as json_error:
+                log.error("Failed to decode message to json: %s.", json_error)
+
+        if consumer.connect():
+            log.info("Successfully connected to Kafka, starting to consume an process messages. ")
+            consumer.read(message_handler=lambda consumer_record: insert_to_tbl(consumer_record.value))
+    except KeyboardInterrupt:
+        print("\n")
+        print("Recived Ctrl-C, closing connection and existing...")
+        consumer.close()
+        client.close()
+        sys.exit(0)
+
 
 if __name__ == "__main__":
-    consumer = Consumer("192.168.99.100:9092", "foobar")
-    consumer.connect()
-    print("------------------------------")
-    consumer.read(message_handler=lambda consumer_record: pprint(consumer_record.value))
-    print("------------------------------")
+    """
+    Entry point of the script  
+    """
 
+    # define command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", help="configuration ini file path.")
+
+    # parse command arguments and read config file
+    args = parser.parse_args()
+    try:
+        config_file_path = os.path.abspath(args.config)
+        log.debug("config file argument value: '%s'", config_file_path)
+
+        postgresql_config = settings.get_config(config_file_path, "postgresql")
+        kafka_config = settings.get_config(config_file_path, "kafka")
+
+        # TODO - do not print password to screen
+        log.debug("postgresql_config %s", postgresql_config)
+        log.debug("kafka_config %s", kafka_config)
+
+        # start reporting to postgres db while consuming form Kafka
+        start_kafka_consumer(kafka_config, postgresql_config)
+    except ConfigurationFileNotFoundException as error:
+        log.error("An error occurred, %s", error)
 
--- scripts/producer.py
+++ scripts/producer.py
@@ -1,30 +1,85 @@
+import argparse
+import os
 import time
 import json
 import logging
 import sys
 
-from kafka_helper.producer import Producer
-from web_health_monitor.web_monitor import HealthMonitor
+from kafka_postgres.settings import settings
+from kafka_postgres.kafka_helper.producer import Producer
+from kafka_postgres.settings.exceptions import ConfigurationFileNotFoundException
+from kafka_postgres.web_health_monitor.web_monitor import HealthMonitor
 
 log = logging.getLogger("kafka_producer_script")
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
+
+
+def start_kafka_producer(kafka_config: dict, web_monitor_config: dict):
+    try:
+        try:
+            producer = Producer(**kafka_config)
+        except KeyError as kw_error:
+            producer.close()
+            log.error("Configuration value missing from config file:  %s", kw_error)
+
+        if producer.connect():
+            log.info("Successfully connected to Kafka, starting to consume an process messages. ")
+        if not producer.connected:
+            log.error("producer is not connected to Kafka server, existing ...")
+            sys.exit(1)
+
+        health_monitor = HealthMonitor(**web_monitor_config)
+
+        while True:
+            data = health_monitor.check()
+            log.debug("message: %s", json.dumps(data))
+            producer.send(json.dumps(data))
+
+            # TODO flush on given bulk count and not every message!
+            #  plus this private and  should not be done from here!
+            producer._producer.flush()
+
+            # TODO sleep should be configrable similar as flush bulk count
+            time.sleep(10)
+
+    except KeyboardInterrupt:
+        print("\n")
+        print("Recived Ctrl-C, closing connection and existing...")
+        producer.close()
+        sys.exit(0)
+
 
 if __name__ == "__main__":
-    producer = Producer("192.168.99.100:9092", "foobar")
+    """
+    Entry point of the script  
+    """
 
-    producer.connect()
-    if not producer.connected:
-        log.error("producer is not connected to Kafka server, existing ...")
-        sys.exit(1)
+    # define command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", help="configuration ini file path.")
 
-    health_monitor = HealthMonitor("https://www.yahoo.com", "get", "(yahoo).+")
-    while True:
-        message = health_monitor.check()
-        from pprint import  pprint
-        print("------------------------------")
-        pprint(message)
-        ret = producer.send(json.dumps(message))
-        print(ret)
-        print("------------------------------")
-        time.sleep(10)
+    # parse command arguments and read config file
+    args = parser.parse_args()
+    config_file_path = os.path.abspath(args.config)
+
+    try:
+        config_file_path = os.path.abspath(args.config)
+        log.debug("config file argument value: '%s'", config_file_path)
+
+        web_monitor_config = settings.get_config(config_file_path, "web_monitor")
+        kafka_config = settings.get_config(config_file_path, "kafka")
+
+        # TODO - do not print password to screen
+        log.debug("web_monitor_config %s", web_monitor_config)
+        log.debug("kafka_config %s", kafka_config)
+
+        # start reporting to postgres db while consuming form Kafka
+        start_kafka_producer(kafka_config, web_monitor_config)
+    except ConfigurationFileNotFoundException as error:
+        log.error("An error occurred, %s", error)
+
+
+
+
 
 
