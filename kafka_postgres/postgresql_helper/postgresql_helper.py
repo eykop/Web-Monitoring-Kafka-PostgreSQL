b8c993c1337197514882cf43077597a23df50f6e
eykop <e.kopty@yahoo.com>
2020-05-10 10:54:12 +0200

    implement bulk insertion to table with unit tests

--- kafka_postgres/postgresql_helper/postgresql_helper.py
+++ kafka_postgres/postgresql_helper/postgresql_helper.py
@@ -7,28 +7,53 @@ some of the code is based on the tutorial form https://www.postgresqltutorial.co
 import psycopg2
 import logging
 
+from kafka_postgres.postgresql_helper.exceptions import DataBaseOperationError
+from kafka_postgres.definitions.keys import MessageJsonKeys
+
 log = logging.getLogger("postgresql_client_helper")
 
 
 class PostgreSqlClient:
     """PostGreSQL simple client"""
 
-    def __init__(self):
-        # the posgresql connection
-        self._connection = None
-
-    def connect(self, host: str, database: str, user: str, password: str) -> bool:
+    def __init__(self,
+                 host: str,
+                 database: str,
+                 user: str,
+                 password: str,
+                 table_name: str,
+                 bulk_count_insert: int = 10):
         """
-        Initialize the client and connects to PostgreSQL server.
+        Initialize the client.
         :param host: database server address e.g., localhost or an IP address
         :param database: the name of the database that you want to connect.
         :param user: the username used to authenticate.
         :param password: password used to authenticate.
+        :param table_name: the db table name to perform the queries on.
+        :param bulk_count_insert: if to perform a bulk insert or single insert(in real case this needs to be a large
+        number something like 1000 to be useful).
+        """
+        self._host = host
+        self._database = database
+        self._user = user
+        self._password = password
+        self._table_name = table_name
+        self._bulk_count_insert = bulk_count_insert
+        self._bulk_list = []
+        self._connection = None
+        self._cursor = None
+
+    def connect(self) -> bool:
+        """
+        Connects to PostgreSQL server.
         :returns bool: True if connection is established , False otherwise.
         """
         try:
 
-            self._connection = psycopg2.connect(host=host, database=database, user=user, password=password)
+            self._connection = psycopg2.connect(host=self._host,
+                                                database=self._database,
+                                                user=self._user,
+                                                password=self._password)
 
             # create a cursor
             self._cursor = self._connection.cursor()
@@ -40,26 +65,23 @@ class PostgreSqlClient:
             db_version = self._cursor.fetchone()
             log.debug('PostgreSQL database version: %s', db_version)
 
-            log.info("Successfully connected to PosgreSQL server %s, version %s", host, db_version)
+            log.info("Successfully connected to PostgreSQL server %s, version %s", self._host, db_version)
 
         except (Exception, psycopg2.DatabaseError) as error:
             log.error("Error occurred when connecting to PostgreSQL server %s, details: %s",
-                      host, error)
+                      self._host, error)
 
         return self.connected
 
     @property
     def connected(self) -> bool:
-        """Check if postgresql client is connected or not, returns matching connected status as boolean"""
+        """Check if PostgreSQL client is connected or not, returns matching connected status as boolean"""
         return self._connection is not None
 
-    def create_web_monitoring_data_base(self, data_base_name):
-        pass
-
-    def create_web_monitoring_table(self, website_name):
+    def create_web_monitoring_table(self):
         command = (
             f"""
-            CREATE TABLE {website_name} (
+            CREATE TABLE {self._table_name} (
                 request_id SERIAL PRIMARY KEY,
                 url VARCHAR(255) NOT NULL,
                 status_code NUMERIC ,
@@ -73,26 +95,36 @@ class PostgreSqlClient:
             """)
         try:
             self.execute_query(command)
-            log.info("Sql table %s created.", website_name)
+            log.info("Sql table %s created.", self._table_name)
         except psycopg2.errors.DuplicateTable as error:
-            log.info("Sql table %s exists.", website_name)
+            log.warning("Sql table %s exists: %s.", self._table_name, error)
 
-    def insert_web_monitoring_result(self, table: str, result: dict):
-        """ insert a new webcheck into the vendors table """
-        sql = f"""INSERT INTO {table}(url, status_code, status_ok, response_time, check_method, 
-                    pattern_matched, requested_pattern, matched_text) VALUES(%s,%s,%s,%s,%s,%s,%s,%s) 
-                  RETURNING request_id;"""
+    def bulk_insert_monitoring_results(self, result: dict):
+        """Inserts a bulk of results to db table at once if a predefined bulk count is reached.
+        :param result: the wbe check result.
+        """
+        self._bulk_list.append((result[MessageJsonKeys.URL],
+                                result[MessageJsonKeys.STATUS_CODE],
+                                result[MessageJsonKeys.STATUS_CODE] != 200,
+                                result[MessageJsonKeys.RESPONSE_TIME_SECS],
+                                result[MessageJsonKeys.METHOD],
+                                result[MessageJsonKeys.IS_PATTER_FOUND],
+                                result[MessageJsonKeys.PATTERN],
+                                result[MessageJsonKeys.MATCHES][:255]))
+
+        current_list_size = len(self._bulk_list)
+
+        if  current_list_size < self._bulk_count_insert:
+            log.debug("Bulk insert limit to web monitoring table %s , not reached yet current %d - required %d .",
+                      self._table_name,
+                      current_list_size,
+                      self._bulk_count_insert)
+            return
+        """ insert a new web check into the vendors table """
+        sql = f"""INSERT INTO {self._table_name}(url, status_code, status_ok, response_time, check_method, 
+                            pattern_matched, requested_pattern, matched_text) VALUES (%s)"""
         try:
-            self._cursor.execute(sql,
-                                 (result["url"],
-                                  result["status_code"],
-                                  result["status_code"] != 200,
-                                  result["response_time_in_sec"],
-                                  result["method"],
-                                  result["pattern_found"],
-                                  result["pattern"],
-                                  result["matches"][:255])
-                                 )
+            self._cursor.execute(sql, self._bulk_list)
             self._connection.commit()
         except (psycopg2.errors.DatatypeMismatch,
                 psycopg2.errors.InvalidTextRepresentation,
@@ -100,9 +132,41 @@ class PostgreSqlClient:
                 psycopg2.errors.NotNullViolation,
                 TypeError) as error:
             log.error("An error occurred during insertion of result to data base table table %s , details: %s",
-                      table, error)
+                      self._table_name, error)
+            raise DataBaseOperationError(error)
+
+    def insert_single_web_monitoring_result(self, result: dict):
+        """ insert a new web check into the vendors table """
+        sql = f"""INSERT INTO {self._table_name}(url, status_code, status_ok, response_time, check_method, 
+                    pattern_matched, requested_pattern, matched_text) VALUES(%s,%s,%s,%s,%s,%s,%s,%s) 
+                  RETURNING request_id;"""
+        try:
+            self._cursor.execute(sql,
+                                 (result[MessageJsonKeys.URL],
+                                  result[MessageJsonKeys.STATUS_CODE],
+                                  result[MessageJsonKeys.STATUS_CODE] != 200,
+                                  result[MessageJsonKeys.RESPONSE_TIME_SECS],
+                                  result[MessageJsonKeys.METHOD],
+                                  result[MessageJsonKeys.IS_PATTER_FOUND],
+                                  result[MessageJsonKeys.PATTERN],
+                                  result[MessageJsonKeys.MATCHES][:255])
+                                 )
+            self._connection.commit()
+        except (psycopg2.errors.DatatypeMismatch,
+                psycopg2.errors.InvalidTextRepresentation,
+                psycopg2.errors.UndefinedColumn,
+                psycopg2.errors.NotNullViolation,
+                psycopg2.errors.UndefinedTable,
+                TypeError) as error:
+            log.error("An error occurred during insertion of result to data base table table %s , details: %s",
+                      self._table_name, error)
+            raise DataBaseOperationError(error)
 
     def execute_query(self, query):
+        """
+        Executes an sql query
+        :param query: the query to execute
+        """
         self._cursor.execute(query)
         # commit the changes
         self._connection.commit()
--- kafka_postgres/postgresql_helper/tests/test_postgresql_helper.py
+++ kafka_postgres/postgresql_helper/tests/test_postgresql_helper.py
@@ -1,60 +1,201 @@
+"""Module that help testing PostgreSQLClient"""
 import unittest
+from unittest import mock
 
+import psycopg2
+
+from kafka_postgres.definitions.keys import MessageJsonKeys
+from kafka_postgres.postgresql_helper.exceptions import DataBaseOperationError
 from ..postgresql_helper import PostgreSqlClient
+
+
 class PostgreSQLClientTest(unittest.TestCase):
+    """The PostgreSQLClient tester class"""
+
+    def setUp(self) -> None:
+        self._test_result = {
+            MessageJsonKeys.MATCHES: 'yahoo.com"><link rel="preconnect" ',
+            MessageJsonKeys.METHOD: 'get',
+            MessageJsonKeys.PATTERN: "re.compile('(yahoo).+')",
+            MessageJsonKeys.IS_PATTER_FOUND: True,
+            MessageJsonKeys.RESPONSE_TIME_SECS: 0.317258,
+            MessageJsonKeys.STATUS_CODE: 200,
+            MessageJsonKeys.STATUS_CODE_OK: True,
+            MessageJsonKeys.URL: 'https://www.yahoo.com'
+        }
 
     def test_connection_success(self):
         """"Tests successful connection"""
-        client = PostgreSqlClient()
-        client.connect("192.168.99.100", "web_monitoring", "postgres", "mysecretpassword")
+        # we build mock from bottom to up
+        cursor_mock = mock.Mock()
+        cursor_mock.execute.side_effect = mock.Mock()
+        cursor_mock.fetchone = mock.Mock()
+        cursor_mock.fetchone.return_value = "Some PSQL version string"
+
+        connect_mock = mock.Mock()
+        connect_mock.cursor.return_value = cursor_mock
+
+        psycopg2.connect = mock.Mock()
+        psycopg2.connect.return_value = connect_mock
+
+        client = PostgreSqlClient("192.168.99.100", "web_monitoring", "postgres", "mysecretpassword", "table_name")
+        self.assertEqual(True, client.connect())
         self.assertEqual(True, client.connected)
+        self.assertEqual(1, len(cursor_mock.execute.mock_calls))
 
     def test_connection_failed_db_not_found(self):
         """"Tests failed connection due to wrong database name """
-        client = PostgreSqlClient()
-        client.connect("192.168.99.100", "nonexistingdb", "postgres", "mysecretpassword")
+        psycopg2.connect = mock.Mock()
+        psycopg2.connect.side_effect = psycopg2.DatabaseError('FATAL:  database "nonexistingdb" does not exist')
+
+        client = PostgreSqlClient("192.168.99.100", "nonexistingdb", "postgres", "mysecretpassword", "table_name")
+        self.assertEqual(False, client.connect())
         self.assertEqual(False, client.connected)
+        self.assertEqual(1, len(psycopg2.connect.mock_calls))
 
     def test_connection_failed_wrong_password(self):
         """"Tests failed connection due to wrong credentials"""
-        client = PostgreSqlClient()
-        client.connect("192.168.99.100", "mydb", "postgres", "bas_password")
+        psycopg2.connect = mock.Mock()
+        psycopg2.connect.side_effect = psycopg2.DatabaseError(
+            'FATAL:  password authentication failed for user "postgres"')
+
+        client = PostgreSqlClient("192.168.99.100", "web_monitoring", "postgres", "bas_password", "table_name")
+        self.assertEqual(False, client.connect())
         self.assertEqual(False, client.connected)
+        self.assertEqual(1, len(psycopg2.connect.mock_calls))
 
     def test_create_table_success(self):
         """Tests creation of db table for web monitoring successful"""
-        client = PostgreSqlClient()
-        client.connect("192.168.99.100", "web_monitoring", "postgres", "mysecretpassword")
+
+        # we build mock from bottom to up
+        cursor_mock = mock.Mock()
+        cursor_mock.execute.side_effect = mock.Mock()
+        cursor_mock.fetchone = mock.Mock()
+        cursor_mock.fetchone.return_value = "Some PSQL version string"
+
+        connect_mock = mock.Mock()
+        connect_mock.cursor.return_value = cursor_mock
+
+        psycopg2.connect = mock.Mock()
+        psycopg2.connect.return_value = connect_mock
+
+        client = PostgreSqlClient("192.168.99.100", "web_monitoring", "postgres", "mysecretpassword", "table_name2")
+        self.assertEqual(True, client.connect())
         self.assertEqual(True, client.connected)
-        client.create_web_monitoring_table(website_name="yahoo")
 
-    def test_insert_web_monitor_result_to_db(self):
-        client = PostgreSqlClient()
-        client.connect("192.168.99.100", "web_monitoring", "postgres", "mysecretpassword")
+        client.create_web_monitoring_table()
+        self.assertEqual(2, len(cursor_mock.execute.mock_calls))
+
+    def test_create_table_failed(self):
+        """Tests creation of db table for web monitoring successful"""
+
+        # we build mock from bottom to up
+        cursor_mock = mock.Mock()
+        cursor_mock.execute.side_effect = [mock.Mock(), psycopg2.errors.DuplicateTable]
+        cursor_mock.fetchone = mock.Mock()
+        cursor_mock.fetchone.return_value = "Some PSQL version string"
+
+        connect_mock = mock.Mock()
+        connect_mock.cursor.return_value = cursor_mock
+
+        psycopg2.connect = mock.Mock()
+        psycopg2.connect.return_value = connect_mock
+
+        client = PostgreSqlClient("192.168.99.100", "web_monitoring", "postgres", "mysecretpassword", "table_name2")
+        self.assertEqual(True, client.connect())
         self.assertEqual(True, client.connected)
-        test_result = {
-            'matches': 'yahoo.com"><link rel="preconnect" '
-                       'href="//csc.beap.bc.yahoo.com"><link rel="dns-prefetch" '
-                       'href="//geo.yahoo.com"><link rel="preconnect" '
-                       'href="//geo.yahoo.com"><link rel="dns-prefetch" '
-                       'href="//comet.yahoo.com"><link rel="preconnect" '
-                       'href="//comet.yahoo.com"><link rel="dns-prefetch" '
-                       'href="//video-api.yql.yahoo.com"><link rel="preconnect" '
-                       'href="//video-api.yql.yahoo.com">    <meta '
-                       'http-equiv="Content-Type" content="text/html; charset=utf-8">',
-            'method': 'get',
-            'pattern': "re.compile('(yahoo).+')",
-            'pattern_found': True,
-            'response_time_in_sec': 0.317258,
-            'status_code': 200,
-            'url': 'https://www.yahoo.com'
-        }
-        client.insert_web_monitoring_result("yahoo", test_result)
+        client.create_web_monitoring_table()
 
+        # expected 2 since we assert the get version when connecting and the create table call
+        self.assertEqual(2, len(cursor_mock.execute.mock_calls))
 
+    def test_insert_web_monitor_result_to_db_success(self):
+        """Tests insertion to table is success"""
+        # we build mock from bottom to up
+        cursor_mock = mock.Mock()
+        cursor_mock.execute.side_effect = mock.Mock()
+        cursor_mock.fetchone = mock.Mock()
+        cursor_mock.fetchone.return_value = "Some PSQL version string"
 
+        connect_mock = mock.Mock()
+        connect_mock.cursor.return_value = cursor_mock
 
+        psycopg2.connect = mock.Mock()
+        psycopg2.connect.return_value = connect_mock
 
+        client = PostgreSqlClient("192.168.99.100", "web_monitoring", "postgres", "mysecretpassword", "table_name23")
+        self.assertEqual(True, client.connect())
+        self.assertEqual(True, client.connected)
+
+        client.insert_single_web_monitoring_result(self._test_result)
+        self.assertEqual(2, len(cursor_mock.execute.mock_calls))
+
+    def test_insert_web_monitor_result_to_db_failed(self):
+        """Tests insertion to table is failed"""
+        # we build mock from bottom to up
+        cursor_mock = mock.Mock()
+        cursor_mock.execute.side_effect = [mock.Mock(), DataBaseOperationError]
+        cursor_mock.fetchone = mock.Mock()
+        cursor_mock.fetchone.return_value = "Some PSQL version string"
+
+        connect_mock = mock.Mock()
+        connect_mock.cursor.return_value = cursor_mock
+
+        psycopg2.connect = mock.Mock()
+        psycopg2.connect.return_value = connect_mock
+
+        client = PostgreSqlClient("192.168.99.100", "web_monitoring", "postgres", "mysecretpassword", "table_name23")
+        self.assertEqual(True, client.connect())
+        self.assertEqual(True, client.connected)
+        with self.assertRaises(DataBaseOperationError):
+            client.insert_single_web_monitoring_result(self._test_result)
+
+    def test_insert_bulk_web_monitor_result_to_db_success(self):
+        """Tests bulk insertion to table is success"""
+        # we build mock from bottom to up
+        cursor_mock = mock.Mock()
+        cursor_mock.execute.side_effect = mock.Mock()
+        cursor_mock.fetchone = mock.Mock()
+        cursor_mock.fetchone.return_value = "Some PSQL version string"
+
+        connect_mock = mock.Mock()
+        connect_mock.cursor.return_value = cursor_mock
+
+        psycopg2.connect = mock.Mock()
+        psycopg2.connect.return_value = connect_mock
+
+        client = PostgreSqlClient("192.168.99.100", "web_monitoring", "postgres", "mysecretpassword", "table_name23", 2)
+        self.assertEqual(True, client.connect())
+        self.assertEqual(True, client.connected)
+
+        # we call twice - our bulk limit for this test is 2 , the insertion will happen in the second call.
+        client.bulk_insert_monitoring_results(self._test_result)
+        client.bulk_insert_monitoring_results(self._test_result)
+        self.assertEqual(2, len(cursor_mock.execute.mock_calls))
+
+    def test_insert_bulk_web_monitor_result_to_db_failed(self):
+        """Tests bulk insertion to table is failed"""
+        # we build mock from bottom to up
+        cursor_mock = mock.Mock()
+        cursor_mock.execute.side_effect = [mock.Mock(), DataBaseOperationError]
+        cursor_mock.fetchone = mock.Mock()
+        cursor_mock.fetchone.return_value = "Some PSQL version string"
+
+        connect_mock = mock.Mock()
+        connect_mock.cursor.return_value = cursor_mock
+
+        psycopg2.connect = mock.Mock()
+        psycopg2.connect.return_value = connect_mock
+
+        client = PostgreSqlClient("192.168.99.100", "web_monitoring", "postgres", "mysecretpassword", "table_name23", 2)
+        self.assertEqual(True, client.connect())
+        self.assertEqual(True, client.connected)
+
+        # first does not trigger - our bulk limit for this test is 2
+        client.bulk_insert_monitoring_results(self._test_result)
+
+        with self.assertRaises(DataBaseOperationError):
+            client.bulk_insert_monitoring_results(self._test_result)
 
 
 if __name__ == '__main__':
