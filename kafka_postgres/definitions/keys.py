a60d60965f5b5b13a98c95b7d1487ec993aef78f
eykop <e.kopty@yahoo.com>
2020-05-10 10:53:20 +0200

    define contants for message json keys

---
+++ kafka_postgres/definitions/keys.py
@@ -0,0 +1,12 @@
+"""Module that help define some constants"""
+
+
+class MessageJsonKeys:
+    MATCHES = 'matches'
+    METHOD = 'method'
+    PATTERN = 'pattern'
+    IS_PATTER_FOUND = 'pattern_found'
+    RESPONSE_TIME_SECS = 'response_time_in_sec'
+    STATUS_CODE = 'status_code'
+    STATUS_CODE_OK = '"status_ok"'
+    URL = 'url'
